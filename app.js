f1f62436c8194de797b6b3ec600da6d725f61715
Megan Flood <mak.flood@comcast.net>
2017-04-10 15:51:01 -0700

    created general store location object with random customer generator and simulation cookies.

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 'use strict';
+
+var pikeStore = {
+  location: '1st and Pike',
+  openAt: 6, // 6 am
+  closeAt: 20, // 8 pm
+  minCustHr: 23,
+  maxCustHr: 65,
+  avgCookieCust: 6.3,
+  cookiesHrs: [],
+
+  // calculates random number of customers for one hour
+  randomCustHr: function() {
+    return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
+  },
+
+  // simulates the number of cookies bought in each hour of a work day
+  simCookiesHrs: function() {
+    this.cookiesHrs = [];
+    for (var i = 0; i <= (this.closeAt - this.openAt); i++) {
+      var cookiesBought = Math.round(this.randomCustHr() * this.avgCookieCust);
+      this.cookiesHrs.push(cookiesBought);
+    }
+  },
+};
